95a5f9078115cd34a23f52fbb30006ff9462f3f5
Zhiying <melody9306@gmail.com>
2023-10-01 23:20:00 +0800

    update BooksController

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -9,114 +9,115 @@ use Illuminate\Http\Request;
 session_start();
 class BooksController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view("index");
     }
-    
-    //儲存亂數
-    public function numRand(){
-        $numRand=array();
-        while(count($numRand)<3){
-            $num=rand(0,9);
-                if(!in_array($num,$numRand)){
-                    $numRand[]=$num;
-                }
+
+    //亂數
+    public function numRand()
+    {
+        $numRand = [];
+        while (count($numRand) < 3) {
+            $num = rand(0, 9);
+            if (!in_array($num, $numRand)) {
+                $numRand[] = $num;
+            }
         }
-    
-        if(session()->has('numRand')){
-            session(['numRand' => $numRand]);
+
+        session(['numRand' => $numRand]);  // 使用 session 函數來存存數據
+
+        if (session()->has('numRand')) {
             return redirect()->route('result');
-        }else{            
-            session(['numRand' => $numRand]);
+        } else {
             return redirect()->route('showRandArr');
         }
     }
 
-    //將亂數變成陣列
-    public function showRandArr(){
+    public function showRandArr()
+    {
         $numRand = session('numRand', []);
-        
-        //儲存結果
-        if(!isset($_SESSION['ans'])){
-            $_SESSION['ans']=array();
+
+        if (!session()->has('ans')) {
+            session(['ans' => []]);
         }
 
-        //計算已進行多少回
-        if(!isset($_SESSION['bout'])){
-            $_SESSION['bout']=0;
-        }
-        else{
-            $_SESSION['bout']+=1;
+        if (!session()->has('bout')) {
+            session(['bout' => 0]);
+        } else {
+            session(['bout' => session('bout') + 1]);
         }
 
-        $numsString= implode(',', $numRand);
-        $_SESSION['ans'][] = $_SESSION['bout']."=".$numsString."<br>"; 
+        $numsString = implode(',', $numRand);
+        session()->push('ans', session('bout') . "=" . $numsString . "<br>");
 
-        if(isset($_SESSION['check'])){
-            if($_SESSION['check']==1){
-                // print_r($_SESSION['ans']);
-                foreach($_SESSION['ans'] as $value){
+        if (session()->has('check')) {
+            if (session('check') == 1) {
+                foreach (session('ans') as $value) {
                     echo $value;
                 }
-            }
-            else{
-                print_r($_SESSION['numRand']);
-                echo "</br>";
-            }
-        }
-        
-        return view ('index');
-    }
-
-    public function result(){
-        $numRand = session('numRand', []);
-        $bout = session('bout',[]);
-        
-        if(!isset($_SESSION['check'])){
-            $_SESSION['check']=0;
-        }
-        else{
-            $_SESSION['check']=1;
-        }
-
-        $abs_AB=abs($_SESSION['numRand'][1]-$_SESSION['numRand'][0]);
-        $abs_BC=abs($_SESSION['numRand'][2]-$_SESSION['numRand'][1]);
-
-        if($_SESSION['bout']<9){
-            if($abs_AB==$abs_BC){
-                $ans.= '總共試了'.$_SESSION['bout'].'次或已找到數字了喔!';
-                session(['ans'=>$ans]);
-                return redirect()->route('insert');
-                return view('index');
+            } else {
+                print_r(session('numRand'));
+                echo "<br>";
             }
         }
-        else{
-            $ans.= '總共試了'.$_SESSION['bout'].'次或已找到數字了喔!';
-            session(['ans'=>$ans]);
-            return redirect()->route('insert');
-            return view('index');
-        }
-    }
 
-    public function insert(){
-        date_default_timezone_set('Asia/Taipei');
-        $freq=$_SESSION['bout'];
-        $data = date('YmdHis');
-    
-        $master = "INSERT INTO mymaster VALUES ('$data', ".($freq+1).")";
-        $result = mysqli_query($link, $master);
-
-        foreach($_SESSION['ansA'] as $i => $value){
-            $num = explode("=", $value)[1];
-            $tail = "INSERT INTO mydetail VALUES ('$data','".($i+1)."','$num')";
-            $result = mysqli_query($link, $tail);
-        }
         return view('index');
     }
-    public function clear(){
-         //清空session內所有資料
-        session()->flush();
-        //重新導回一開始的樣子
+
+    public function result()
+    {
+        $numRand = session('numRand', []);
+        $bout = session('bout', 0);
+
+        if (!session()->has('check')) {
+            session(['check' => 0]);
+        } else {
+            session(['check' => 1]);
+        }
+
+        $abs_AB = abs($numRand[1] - $numRand[0]);
+        $abs_BC = abs($numRand[2] - $numRand[1]);
+
+        if ($bout < 9) {
+            if ($abs_AB == $abs_BC) {
+                $ans = '總共試了' . $bout . '次或已找到數字了喔!';
+                session(['ans' => $ans]);
+                return redirect()->route('insert');
+            }
+        } else {
+            $ans = '總共試了' . $bout . '次或已找到數字了喔!';
+            session(['ans' => $ans]);
+            return redirect()->route('insert');
+        }
+    }
+
+    public function insert()
+    {
+        date_default_timezone_set('Asia/Taipei');
+        $freq = session('bout', 0);
+        $data = date('YmdHis');
+
+        $master = new Mymaster();  // 創建主紀錄
+        $master->id = $data;
+        $master->freq = $freq + 1;
+        $master->save();
+
+        foreach (session('ans') as $i => $value) {
+            $num = explode("=", $value)[1];
+            $detail = new Mydetail();  // 創建詳細記錄
+            $detail->id = $data;
+            $detail->turn = $i + 1;
+            $detail->rec = $num;
+            $detail->save();
+        }
+
+        return view('index');
+    }
+
+    public function clear(Request $request)
+    {
+        $request->session()->flush();  // 清空會話中的所有數據
         return redirect()->route('start');
     }
 }
